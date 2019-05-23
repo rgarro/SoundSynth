d9d613a7e9495135e651b95ef7f3928823a618a1
rgarro <rgarro@gmail.com>
2019-05-23 14:04:09 -0600

    mad house whishing a farewell ...

---
+++ tutorial/tut01/Source/GrassComponent.cpp
@@ -0,0 +1,65 @@
+/*
+  ==============================================================================
+
+    GrassComponent.cpp
+    Created: 23 May 2019 1:49:10pm
+    Author:  Rolando Garro
+        __
+    _.-'  \
+   (-'     \
+    \       \
+     \       \
+      \  _.-' \
+       \'      \
+        \       \
+         \    .-.\_
+          \ .(      )_  _
+          _\( `-..-' )`-.\\
+         /__'`-._.--' .' )/
+VK       \   \- _  .-' .'
+          `---`/.\\_.-'
+               \_//
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "GrassComponent.h"
+
+//==============================================================================
+GrassComponent::GrassComponent()
+{
+    // In your constructor, you should add any child components, and
+    // initialise any special settings that your component needs.
+
+}
+
+GrassComponent::~GrassComponent()
+{
+}
+
+void GrassComponent::paint (Graphics& g)
+{
+    /* This demo code just fills the component's background and
+       draws some placeholder text to get you started.
+
+       You should replace everything in this method with your own
+       drawing code..
+    */
+
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));   // clear the background
+
+    g.setColour (Colours::grey);
+    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
+
+    g.setColour (Colours::white);
+    g.setFont (14.0f);
+    g.drawText ("GrassComponent", getLocalBounds(),
+                Justification::centred, true);   // draw some placeholder text
+}
+
+void GrassComponent::resized()
+{
+    // This method is where you should set the bounds of any child
+    // components that your component contains..
+
+}
---
+++ tutorial/tut01/Source/GrassComponent.h
@@ -0,0 +1,43 @@
+/*
+  ==============================================================================
+
+    GrassComponent.h
+    Created: 23 May 2019 1:49:10pm
+    Author:  Rolando Garro
+        __
+    _.-'  \
+   (-'     \
+    \       \
+     \       \
+      \  _.-' \
+       \'      \
+        \       \
+         \    .-.\_
+          \ .(      )_  _
+          _\( `-..-' )`-.\\
+         /__'`-._.--' .' )/
+VK       \   \- _  .-' .'
+          `---`/.\\_.-'
+               \_//
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+*/
+class GrassComponent    : public Component
+{
+public:
+    GrassComponent();
+    ~GrassComponent();
+
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GrassComponent)
+};
